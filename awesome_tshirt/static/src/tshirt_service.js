1f5be0424ae39db5593ef45f4bc0b6895c78363f
dmetree01 <nevdk1@gmail.com>
2024-10-24 20:34:17 +0300

    .

--- awesome_tshirt/static/src/tshirt_service.js
+++ awesome_tshirt/static/src/tshirt_service.js
@@ -1,6 +1,7 @@
 /** @odoo-module */
 import { registry } from "@web/core/registry";
 import { memoize } from "@web/core/utils/functions";
+
 export const tshirtService = {
     dependencies: ["rpc"],
     async: ["loadStatistics"],
